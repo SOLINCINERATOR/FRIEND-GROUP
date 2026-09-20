ca8a8a29f8edf475272119b76c70e86ba74e884d
SOLINCINERATOR <roveryou8@gmail.com>
2026-09-20 12:38:27 +0200

    Delete members.js

--- members.js
+++
@@ -1,6 +0,0 @@
-/* ==========================================================================
-   members.js – hier stehen deine Freunde.
-   Diese Datei erzeugt das Admin-Panel für dich ("members.js herunterladen").
-   Einfach die heruntergeladene Datei hier drüber kopieren / hochladen.
-   ========================================================================== */
-window.MEMBERS_DATA = [];
